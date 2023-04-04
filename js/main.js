6a1ba06d4c399050ec1e5b28869c0c5c6ab9bdce
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-04 07:56:02 -0300

    Adding instructions exercise two

--- js/main.js
+++ js/main.js
@@ -2,28 +2,20 @@
 
 // Recordá que luego de crear los elementos deben agregarse al documento o a algún otro elemento que se encuentre en el mismo.
 
-// Ejercicio 1 : Creación de nodo.
-// Crear un nodo de tipo h1
-// Asignarle un texto Hola DOM!
-// Colocarle un color, un tamaño de texto y una familia de fuente a elección.
+// Ejercicio 2: Creación de listas
+// Crear un elemento desde el DOM de tipo ol
+
+// Asignarle 3 elementos de tipo li, con lo siguientes textos:
+
+// Home
+// Nosotros
+// Contacto
 
 const create = tag => document.createElement(tag)
 const $ = selector => document.querySelector(selector)
 const $$ = selector => document.querySelectorAll(selector)
 
-const exerciseOne = (value) =>{
-   //Capturing body using querySelector
-   const body = $('body')
-   const h1 = create('h1')
+const exerciseTwo = () =>{
 
-   // I assign the text 'Hola DOM'
-   h1.innerText = value
-
-   //I add the class to the h1
-   h1.classList.add('classExerciseOne')
-
-   //I add the <h1> as a child 
-   body.appendChild(h1)
-   console.log(h1)
 }
-exerciseOne('Hola DOM!')
+exerciseTwo()
