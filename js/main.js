482486d7f61bb57393564eec282f3359fd09617f
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-04 09:22:36 -0300

    resut exercise months

--- js/main.js
+++ js/main.js
@@ -2,41 +2,26 @@
 
 // Recordá que luego de crear los elementos deben agregarse al documento o a algún otro elemento que se encuentre en el mismo.
 
-// Ejercicio 2: Listas anidadas
-// Replica la siguiente estructura HTML creando nodos mediante javascript
+// Ejercicio 3: Botones
+// Por cada ítem de la siguiente lista, crear un botón con el texto de dicho ítem. Al clickear un botón, debe aparecer un alert que diga Has clickeado el mes X, dependiendo del mes clickeado (por ejemplo, Has clickeado el mes: Mayo)
 
-// <ul>
-//   <li>Verduras</li>
-//   <li>Garbanzos</li>
-//   <li>
-//     Frutas
-//     <ul>
-//       <li>Manzanas</li>
-//       <li>Naranjas</li>
-//       <li>Bananas</li>
-//       <li>Frutillas</li>
-//     </ul>
-//   </li>
-// </ul>
+const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] 
+
 
 const create = tag => document.createElement(tag)
 const $ = selector => document.querySelector(selector)
 const $$ = selector => document.querySelectorAll(selector)
 const body = $('body')
 
-const nestedList = () =>{
-   body.innerHTML = `<ul>
-     <li>Verduras</li>
-     <li>Garbanzos</li>
-     <li>
-       Frutas
-       <ul>
-         <li>Manzanas</li>
-         <li>Naranjas</li>
-         <li>Bananas</li>
-         <li>Frutillas</li>
-       </ul>
-     </li>
-   </ul>`
+const buttons = (meses) =>{
+   meses.forEach( month => {
+      const button = create('button')
+      button.innerText += month
+      body.appendChild(button)
+      button.onclick = function(){
+         alert(month)
+      }
+   });
 }
-nestedList()
+buttons(meses)
+
