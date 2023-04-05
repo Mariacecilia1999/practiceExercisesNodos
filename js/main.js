e44851ce3a6cc5eeb5f65c4b2b7b9ca59376da7d
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-05 10:34:03 -0300

    Adding result exercise six

--- js/main.js
+++ js/main.js
@@ -1,1 +1,70 @@
 // Crear una lista desordenada. Por cada ítem del array, crear un ítem de lista con el texto de dicho ítem. Cada vez que se clickea el ítem, debe ir cambiando de color, siguiendo este orden: gris -> celeste -> amarillo -> rojo. Cuando llega al último color y se lo vuelve a clickear, debe volver al primer color.
+
+const tareas = [
+   'Comprar comida',
+   'Estudiar para examen',
+   'Pagar impuestos',
+   'Pasear a perro',
+   'Comprar entradas para el cine',
+ ]
+
+let ul
+let li
+
+const changeColorYellow = (valueThree) =>{
+   const allLi = document.querySelectorAll('li')
+   allLi.forEach(clicLi =>{
+      clicLi.onclick = function(){
+         clicLi.classList.remove('blue')
+         clicLi.classList.add(`${valueThree}`)
+         changeColorRed('red')
+      }
+   })
+ }
+
+
+
+
+const changeColorBlue = (valueTwo) =>{
+   const allLi = document.querySelectorAll('li')
+   allLi.forEach(clicLi =>{
+      clicLi.onclick = function(){
+         clicLi.classList.remove('red')
+         clicLi.classList.add(`${valueTwo}`)
+         changeColorYellow('yellow')
+      }
+   })
+ }
+
+const changeColorRed = (valueOne) =>{
+   const allLi = document.querySelectorAll('li')
+   allLi.forEach(clicLi =>{
+      clicLi.onclick = function(){
+         clicLi.classList.remove('yellow')
+         clicLi.classList.add(`${valueOne}`)
+         changeColorBlue('blue')
+      }
+   })
+ }
+
+
+
+
+
+ const listTask = (values) =>{
+   const body = document.querySelector('body')
+    ul = document.createElement('ul')
+   body.appendChild(ul)
+   values.forEach(value => {
+      li = document.createElement('li')
+      ul.appendChild(li)
+      li.innerText=value
+      changeColorRed('red')
+      
+   });
+
+ }
+ listTask(tareas)
+
+
+
