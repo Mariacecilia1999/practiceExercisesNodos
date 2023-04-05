e8430374a1efd063de1b1d06be90eed263a4bd10
Mariacecilia1999 <conticecilia199@gamil.com>
2023-04-05 09:11:58 -0300

    Adding a <li> for each item

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 // Crear una lista desordenada. Por cada ítem del array, crear un ítem de lista con el texto de dicho ítem y un botón que diga Eliminar. Al clickear el botón, se debe eliminar de la lista el nodo correspondiente.
+
+
+const tareas = [
+   'Comprar comida',
+   'Estudiar para examen',
+   'Pagar impuestos',
+   'Pasear a perro',
+   'Comprar entradas para el cine',
+ ]
+
+
+const tasks = (values) =>{
+   const body = document.querySelector('body')
+   const ul = document.createElement('ul')
+   body.appendChild(ul)
+   for(const task of values){
+      const li = document.createElement('li')
+      li.innerHTML += task
+      ul.appendChild(li)
+   }
+}
+tasks(tareas)
